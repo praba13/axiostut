f435d4f8d93b585b8a07e1db473d747a51bf5af5
Praba <prama1303@gmail.com>
2022-12-26 22:26:44 +0100

    CRUD axios with json placeholder

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,62 @@ var router = express.Router();
 const axios = require('axios');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  const idVal = req.query.id;
+  /*
+   axios
+    .get('https://jsonplaceholder.typicode.com/todos/5')
+    .then(function (response) {
+      res.render('index', { title: response.data.title });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    */
+
+  axios
+    .get('https://jsonplaceholder.typicode.com/todos/', {
+      params: { id: idVal }
+    })
+    .then(function (response) {
+      //res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  axios
+    .post('https://jsonplaceholder.typicode.com/posts', {
+      title: 'PR-Axios',
+      body: 'PR-Body'
+    })
+    .then(function (response) {
+      //res.send(response.data);
+    })
+    .catch(function (e) {
+      console.log(e);
+    });
+
+  axios
+    .put('https://jsonplaceholder.typicode.com/posts/' + idVal, {
+      title: 'PR-Axios Updated',
+      body: 'PR-Body Updated'
+    })
+    .then(function (response) {
+      //res.send(response.data);
+    })
+    .catch(function (e) {
+      console.log(e);
+    });
+
+  axios
+    .delete('https://jsonplaceholder.typicode.com/posts/1')
+    .then(function (response) {
+      res.send('Data Deleted successfully');
+    })
+    .catch(function (e) {
+      console.log(e);
+    });
 });
 
 module.exports = router;
